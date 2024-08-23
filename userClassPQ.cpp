1310afe76b29d028c7299825f3573c3a8fb87143
srshoruv <srshoruv2002@gmail.com>
2024-08-23 21:31:07 +0600

    practice problem solved

---
+++ userClassPQ.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class User{
+    int id;
+    string password;
+public:
+    string userName;
+    User (int id) {
+        this->id = id;
+    } 
+
+    void setPass(string password){
+            this->password = password;
+        }
+        string getPass(){
+            return password;
+        }
+};
+
+int main() {
+    User u1(7);
+    u1.userName = "srshoruv";
+    u1.setPass("srshoruv30");
+
+    cout << u1.getPass();
+}
